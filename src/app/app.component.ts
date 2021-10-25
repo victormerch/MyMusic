6f061bbfb2a753280befd6909f49f87b9261b40c
Victor Merchan <73888267+victormerch@users.noreply.github.com>
2021-10-25 12:52:51 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'MyMusic-a';
+  
+  title:string = 'MyMusic';
+  name:string = "Victor MV";
+  age:number = 18;
+  relationShip:undefined = undefined;
+  datum:any = "Este tipo de datos puede contener cualquier tipo de dato"
+
 }
