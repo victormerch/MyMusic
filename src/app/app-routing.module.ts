024ffbebb7d2c0257b62524f9503afe23dbe894d
Victor Merchan <73888267+victormerch@users.noreply.github.com>
2021-10-27 09:58:58 +0200

    tercer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', //TODO:localhost:4200/ Componente de prueba
+    component: ExampleComponent
+  },
+  {
+    path:'prueba', //TODO:localhost:4200/prueba Componente de prueba
+    component: ExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
