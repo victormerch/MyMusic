a87b7b7598eb9ccae5291436c141714d30e5d5f5
Victor Merchan <73888267+victormerch@users.noreply.github.com>
2021-10-27 12:49:40 +0200

    commit cuatro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
+
 
 const routes: Routes = [
-  {
-    path:'', //TODO:localhost:4200/ Componente de prueba
-    component: ExampleComponent
-  },
-  {
-    path:'prueba', //TODO:localhost:4200/prueba Componente de prueba
-    component: ExampleComponent
-  }
+  // {
+  //   path:'', //TODO:localhost:4200/ Componente de prueba
+  //   component: ExampleComponent
+  // },
+  // {
+  //   path:'prueba', //TODO:localhost:4200/prueba Componente de prueba
+  //   component: ExampleComponent
+  // }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExampleComponent
+  declarations: [//TODO: Declaraciones, componentes directivas pipes
+    AppComponent
   ],
-  imports: [
+  imports: [//TODO: SOlo se importan otros modules
     BrowserModule,
     AppRoutingModule
   ],
