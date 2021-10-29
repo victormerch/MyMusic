886c99dc8e52acb97b092ece9bc8723f8ea1be06
Victor Merchan <73888267+victormerch@users.noreply.github.com>
2021-10-29 11:18:01 +0200

    actulalizacion de routing model de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
+  {
+    path : '',//TODO: localhost:4200/
+    loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)//La tuta raiz va a cargar el modulo home.module
+  }
+  
   // {
   //   path:'', //TODO:localhost:4200/ Componente de prueba
   //   component: ExampleComponent
